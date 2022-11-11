08b97c09e26e0d8cd8d240c8d95284d09e5caf5e
MSI\KAI <nhanksd85@gmail.com>
2022-11-11 13:38:56 +0700

    Add source code

--- app/src/main/java/ads/movies/npnlab/ads/MainActivity.java
+++ app/src/main/java/ads/movies/npnlab/ads/MainActivity.java
@@ -1,21 +1,64 @@
 package ads.movies.npnlab.ads;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.videoViewAdvertisement)
+    VideoView mVideoView;
+
+    @BindView(R.id.imgAdvertisement)
+    ImageView imgAdvertisement;
+
+
+    private int currentAdFileIndex = -1;
+
+    private int currentImageFileIndex = 0;
+
+    public String roothPath = "";
+    public String match_folder = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getUSB();
+        roothPath = getUSB();
+
+        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                //TODO: move to next video
+                if(name_img_path_list.size() > 0){
+
+                    currentImageFileIndex = 0;
+
+                }else {
+                    if (name_path_list.size() > 0) {
+                        currentAdFileIndex++;
+                        if (currentAdFileIndex >= name_path_list.size()) currentAdFileIndex = 0;
+                        playVideoFromUSB(name_path_list.get(currentAdFileIndex));
+
+                    }
+                }
+
+
+            }
+        });
     }
 
     public String getUSB(){
@@ -70,6 +113,97 @@ public class MainActivity extends AppCompatActivity {
         }
         if(action.indexOf("MEDIA_REMOVED") >=0){
 
+        }
+    }
+
+    public String findAdvertisementFolder(File dir){
+        File[] listFile = dir.listFiles();
+        if (listFile != null) {
+            for (int i = 0; i < listFile.length; i++) {
+                if (listFile[i].isDirectory()) {
+                    if (listFile[i].getAbsolutePath().toLowerCase().indexOf(match_folder) >= 0) {
+                        return listFile[i].getAbsolutePath();
+                    }
+                }
+            }
+            for (int i = 0; i < listFile.length; i++) {
+                if (listFile[i].isDirectory()) {
+                    return  findAdvertisementFolder(listFile[i]);
+                }
+            }
+            return "";
+        }else{
+            return "";
+        }
+    }
+
+
+    ArrayList<String> name_list = new ArrayList<>();
+    ArrayList<String> name_path_list = new ArrayList<>();
+
+    ArrayList<String> name_img_list = new ArrayList<>();
+    ArrayList<String> name_img_path_list = new ArrayList<>();
+
+    private void load_img_files(File dir){
+        name_img_list.clear();
+        name_img_path_list.clear();
+
+        File[] listFile = dir.listFiles();
+
+
+        if (listFile != null) {
+            for (int i = 0; i < listFile.length; i++) {
+
+                if (listFile[i].isDirectory()) {
+                    load_img_files(listFile[i]);
+                } else {
+                    if (listFile[i].getName().endsWith("jpg") || listFile[i].getName().endsWith("bmp")
+                            || listFile[i].getName().endsWith("png")) {
+                        name_img_list.add(listFile[i].getName());
+                        name_img_path_list.add(listFile[i].getAbsolutePath());
+                        currentImageFileIndex = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    private void load_ad_files(File dir) {
+
+        name_list.clear();
+        name_path_list.clear();
+        String extention = ".mp4";
+        File[] listFile = dir.listFiles();
+
+
+        if (listFile != null) {
+            for (int i = 0; i < listFile.length; i++) {
+
+                if (listFile[i].isDirectory()) {
+                    load_ad_files(listFile[i]);
+                } else {
+                    if (listFile[i].getName().endsWith(extention)) {
+                        name_list.add(listFile[i].getName());
+                        name_path_list.add(listFile[i].getAbsolutePath());
+
+                    }
+                }
+            }
+        }
+    }
+
+    public void playVideoFromUSB(String path){
+        isActivatedImageAnimation = false;
+        File aFile = new File(path);
+        if(aFile.exists()){
+            imgAdvertisement.setVisibility(View.GONE);
+            mVideoView.setVisibility(View.VISIBLE);
+            mVideoView.setVideoURI(Uri.parse(path));
+            mVideoView.start();
+        }else{
+            Log.d("DCAR", "Video is not existed");
+            ((MainActivity)getActivity()).fullScreenMode(false);
+            ((MainActivity)getActivity()).selectFragment(HOME_FRAGMENT_INDEX);
         }
     }
 }
